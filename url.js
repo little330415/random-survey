8cd184c5a6d85b25069a3d48377ebfaee868dade
little330415 <little330415@gmail.com>
2025-12-21 21:04:24 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,6 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232',
+	'https://forms.gle/7yQnKnJcFW6Zfkvh8',
+	'https://forms.gle/ZjnUJNVH3Bs8b6tB7'
 ];
